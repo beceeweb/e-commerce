01eb697371927954dc43f1e6ccf4c2ce29c3b30f
David <bensadondavidn@gmai.com>
2026-06-10 23:15:04 +0300

    changes

---
+++ app/api/stripe/webhook/route.ts
@@ -0,0 +1,9 @@
+import { stripe } from "@/lib/payment/stripe";
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/database/prisma";
+
+
+
+export async function POST(req: NextRequest){
+    
+}
